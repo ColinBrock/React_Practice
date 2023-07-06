d34201050daa54a77bd8bc184a8eadd6140e07bf
Colin Brock <colinebrock@gmail.com>
2023-07-06 07:04:20 -0500

    first project complete

--- index.js
+++ index.js
@@ -1,26 +1,36 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-/**
-Challenge: fix our code!
+/*
+Challenge: Starting from scratch, build and render the 
+HTML for our section project. Check the Google slide for 
+what you're trying to build.
 
-Don't forget, you're not using CDNs anymore, so there's no
-global "ReactDOM" variable to use anymore.
+We'll be adding styling to it later.
+
+Hints:
+* The React logo is a file in the project tree, so you can
+  access it by using `src="./react-logo.png" in your image
+  element
+* You can also set the `width` attribute of the image element
+  just like in HTML. In the slide, I have it set to 40px
  */
 
-const page = (
+const funFacts = (
     <div>
-        <h1>My awesome website in React</h1>
-        <h3>Reasons I love Peeact</h3>
-        <ol>
-            <li>It's ?</li>
-            <li>It's neat</li>
-            <li>It's *indistinguishable*</li>
-            <li>I don't actually love it</li>
-        </ol>
+    
+    <img src="./react-logo.png" width='40px'/>
+    
+        <h1>Fun facts about React</h1>
+        <ul>
+        <li>OG release in 2013</li>
+        <li>Jordan Walke made it :)</li>
+        <li>Over 100k stars on GH (cool)</li>
+        <li>Facebook rules it and all of us</li>
+        <li>Can control mobile devices and computer devices using the "React Power Core" (RPC)*</li>
+        </ul>
+        <p style={{fontSize:".5rem"}}> *Note some facts may be slightly embellished.</p>
     </div>
 )
-//Did challenge using 18
-document.getElementById("root").append(JSON.stringify(page))
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(page)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(funFacts)
